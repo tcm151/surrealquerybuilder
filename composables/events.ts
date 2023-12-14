b670b351d533e6dbcdc0316f78d2e10ee1070664
tcm <tylermckay10@gmail.com>
2023-12-13 23:29:39 -0400

    add events

---
+++ composables/events.ts
@@ -0,0 +1,32 @@
+import { defineStore } from "pinia"
+
+interface Event {
+    trigger: Trigger
+    action: Function
+}
+
+export enum Trigger {
+    switchedNamespaces
+}
+
+export const useEvents = defineStore("events", () => {
+    let events = ref<Event[]>([]);
+
+    function subscribe(trigger: Trigger, action: Function) {
+        events.value.push({ trigger: trigger, action: action})
+    }
+
+    function unsubscribe(event: Event) {
+        events.value = events.value.filter((e) => e === event)
+    }
+
+    function publish(trigger: Trigger, payload?: object) {
+        events.value.forEach((e) => {
+            if (e.trigger === trigger) {
+                e.action(payload)
+            }
+        })
+    }
+
+    return { subscribe, unsubscribe, publish }
+})
