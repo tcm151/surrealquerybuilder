fc84bf20a161dda0472ead2986cf1bc2b3b6d328
tcm <tylermckay10@gmail.com>
2023-12-15 15:47:02 -0400

    simplify cache

--- composables/cache.ts
+++ composables/cache.ts
@@ -10,35 +10,22 @@ export const useCache = defineStore("cache", () => {
     const { settings } = storeToRefs(useSettings())
 
     let cache = useLocalStorage<Cache>(`cache:${settings.value.namespace}`, {})
-    events.subscribe(Trigger.switchedNamespaces, () => {
-        cache = useLocalStorage<Cache>(`cache:${settings.value.namespace}`, {})
-    })
 
-    function get<T>(key: string, fallback?: () => T): Ref<T> {
-        // WARN potential for performance degradations here
+    function get<T>(key: string, fallback: () => T): Ref<T> {
         if (cache.value[key] != undefined) {
             const cachedRef = ref<T>(cache.value[key]) as Ref<T>
+            // WARN potential for performance degradations here
             watch (cachedRef, () => cache.value[key] = cachedRef.value)
             return cachedRef
         }
-        else if (fallback) {
+        else {
             cache.value[key] = fallback()
             const fallbackRef = ref<T>(cache.value[key]) as Ref<T>
+            // WARN potential for performance degradations here
             watch (fallbackRef, () => cache.value[key] = fallbackRef.value)
             return fallbackRef
         }
-        else {
-            throw createError({
-                fatal: true,
-                statusCode: 404,
-                message: "This value was not cached, and no fallback was provided."
-            })
-        }
     }
 
-    function set(key: string, value: any) {
-        cache.value[key] = value
-    }
-
-    return { get, set }
+    return { get }
 })
